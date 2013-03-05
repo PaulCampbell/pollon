d82fc148e9ef979643b3cf5fb8dcd4a96bb7d323
paulcampbell <paulgcampbell@gmail.com>
2013-03-05 07:50:05 +0000

    few fixes -ab test example model added.

--- infrastructure/routes.js
+++ infrastructure/routes.js
@@ -1,6 +1,7 @@
 var  user = require('../actions/user')
   , home = require('../actions/home')
   , account = require('../actions/account')
+  , ab_test = require('../actions/ab_test')
   , authentication = require('./authentication.js')
 
 function init(app){
@@ -16,9 +17,12 @@ app.get('/forgotten-password', account.forgottenPassword);
 app.post('/request-password', account.passwordRequest);
 app.get('/change-password/:token', account.changePassword);
 app.post('/change-password', account.changePasswordRequest);
-app.post('/authedPasswordChange', account.authedPasswordChange, account.accountSettingsForm)
+app.post('/authedPasswordChange',authentication.checkAuth, account.authedPasswordChange)
 app.get('/account-settings',authentication.checkAuth, account.accountSettingsForm);
 app.post('/account-settings',authentication.checkAuth, account.accountSettings);
+
+app.get('/tests/new', authentication.checkAuth, ab_test.addTestForm)
+app.post('/tests', authentication.checkAuth, ab_test.addTest)
 }
 
 exports.init = init;
--- test/models.coffee
+++ test/models.coffee
@@ -3,6 +3,7 @@ zombie = require 'zombie'
 Users = require '../Models/User.js'
 PasswordResets = require '../Models/PasswordReset.js'
 mongoose = require 'mongoose'
+ABTests = require '../Models/ABTest.js'
 
 user = null
 password_reset = null
@@ -42,3 +43,10 @@ describe 'PasswordReset', ->
     password_reset.isValid.should.equal false
     done()
 
+
+
+describe 'ABTest', ->
+  it 'must have a name', (done) ->
+    ab_test = new ABTests.ABTest({page_a:'http://localhost:8000/pagea.html'});
+    console.log(ab_test)
+    done()
